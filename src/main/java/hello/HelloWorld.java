580efaf1bc6da2ffbd3d82055b68643d0e890b73
JustLucky777 <timka777@mail.ru>
2017-04-13 00:26:42 +0600

    commit 1

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.hello;
+
+/**
+ * Created by xCot on 30-Mar-17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
